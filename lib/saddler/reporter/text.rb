92e8df274d57e0c1c9b1a91197b6f8e92d7dd2e4
sanemat <o.gata.ken@gmail.com>
2015-02-18 17:49:22 +0900

    feat(reporter): add implementation

--- lib/saddler/reporter/text.rb
+++ lib/saddler/reporter/text.rb
@@ -1,9 +1,15 @@
-require 'saddler/reporter/text/version'
-
 module Saddler
   module Reporter
-    module Text
+    class Text
+      VERSION = '0.1.0'
       # Your code goes here...
+      def initialize(output)
+        @output = output
+      end
+
+      def report(messages, options)
+        @output.puts messages
+      end
     end
   end
 end
--- lib/saddler/reporter/text/version.rb
+++
@@ -1,7 +0,0 @@
-module Saddler
-  module Reporter
-    module Text
-      VERSION = '0.1.0'
-    end
-  end
-end
