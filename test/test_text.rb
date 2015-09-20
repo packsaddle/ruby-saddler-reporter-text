641348c08e201c970d65b9d86feb1e95a93d5853
sanemat <o.gata.ken@gmail.com>
2015-09-20 22:27:57 +0900

    test(parse): test parsing normal file

--- test/test_text.rb
+++ test/test_text.rb
@@ -8,6 +8,26 @@ module Saddler
           !::Saddler::Reporter::Text::VERSION.nil?
         end
       end
+
+      sub_test_case '.parse' do
+        too_long = File.read('./test/fixtures/too_long.xml')
+        reporter = Text.new(StringIO.new)
+        parsed = [
+          {
+            file: 'lib/example/travis_ci.rb',
+            line: 7,
+            column: 120,
+            severity: 'info',
+            message: 'Line is too long. [164/120]',
+            source: 'com.puppycrawl.tools.checkstyle.Metrics/LineLength'
+          }
+        ]
+        test 'too long' do
+          assert do
+            reporter.parse_messages(too_long) == parsed
+          end
+        end
+      end
     end
   end
 end
